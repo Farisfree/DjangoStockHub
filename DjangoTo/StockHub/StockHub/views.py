acf219bb90ac1b0319f042980b9c52af12b30b37
LillardWu <247480289@qq.com>
2023-05-04 22:36:27 +0800

    111

--- DjangoTo/StockHub/StockHub/views.py
+++ DjangoTo/StockHub/StockHub/views.py
@@ -27,14 +27,19 @@ def login(request):
         tmp_id = request.POST.get("user_id")
         password = request.POST.get("user_password")
 
-        check = cursor.execute(f"select * from people where user_id = {tmp_id} and passwd = {password}")
+        check = cursor.execute(f"select user_type from people where user_id = {tmp_id} and passwd = {password}")
 
         if check:
             info = cursor.fetchall()
-            type = int(info[0][1])
 
-            global user_id, user_type
-            user_id= tmp_id, user_type = type
+            tmp_type = int(info[0][0])
+            tmp_type = str(tmp_type)
+
+
+            global user_id
+            user_id= tmp_id
+            global user_type
+            user_type = tmp_type
 
             return render(request, "home.html")
         else:
@@ -48,6 +53,7 @@ def search(request):
         return render(request, "search.html", {"user_type": user_type})
 
     stock_code = request.POST.get('stock_code')
+    code_check = cursor.execute(f'select * from ')
     stock_name = request.POST.get('stock_name')
     # 这里搜索成功后就是跳转到展示信息的页面
     if stock_code:
@@ -60,6 +66,8 @@ def search(request):
         info = cursor.fetchall()
         data = pd.DataFrameinfo()
         return HttpResponse("以名字搜索股票")
+    else:
+        return HttpResponse("输入错误")
 
 
 def register(request):
@@ -75,7 +83,7 @@ def register(request):
         else:
             cursor.execute(f'insert into people(user_id, passwd) values ({user_id},{user_password})')
             # 这里你看看是返回到登录界面还是直接跳到主界面
-            return HttpResponse("注册成功")
+            return render(request, "home.html")
 
 
 # 注意体一下，在测试的时候 全局变量type 要设置成为字符串的形式(改int也行)
@@ -87,12 +95,12 @@ def delete(request):
             return render(request, 'tmp.html')
 
     else:
-        user_id = request.POST.get("user_id")
-        user_password = request.POST.get("user_password")
+        id = request.POST.get("user_id")
+        password = request.POST.get("user_password")
 
-        check = cursor.execute(f'select * from people where user_id = {user_id} and passwd = {user_password}')
+        check = cursor.execute(f'select * from people where user_id = {id} and passwd = {password}')
         if check:
-            cursor.execute(f'delete from people where user_id = {user_id}')
+            cursor.execute(f'delete from people where user_id = {id}')
             return render(request, 'delete.html')
         else:
             # 加一个删除失败的跳窗在 delete的界面上
