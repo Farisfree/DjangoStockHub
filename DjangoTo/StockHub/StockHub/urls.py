8c803c74777b24d003d13a003415a192b3d82c2f
LillardWu <247480289@qq.com>
2023-05-06 21:39:32 +0800

    search做了一个跳转 名为search_list的界面

--- DjangoTo/StockHub/StockHub/urls.py
+++ DjangoTo/StockHub/StockHub/urls.py
@@ -2,11 +2,25 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #path('login/', views.login),
     path('login/', views.login),
-    path('', views.login),
-    path('search/', views.search),
+    # path('search/', views.search),
     path('register/',views.register),
     path('delete/', views.delete),
     path('home/',views.home),
+    # path('searchlist/', views.search_list),
+    path('', views.search_list),
+
+    path('stock_basic_info/', views.stock_basic_info),
+    path('stock_daily_data/', views.stock_daily_data),
+    path('stock_dividend_data/',views.stock_dividend_data),
+    path('stock_fees_data/', views.stock_fees_data),
+    path('stock_financial_data/', views.stock_financial_data),
+    path('stock_price_data/', views.stock_price_data),
+    path('stock_ratios_data/', views.stock_ratios_data),
+    path('stock_return_data/', views.stock_return_data),
+    path('stock_shares_data/', views.stock_shares_data),
+
+    path('test/', views.test),
 
 ]
--- DjangoTo/StockHub/StockHub/views.py
+++ DjangoTo/StockHub/StockHub/views.py
@@ -8,15 +8,19 @@ import pandas as pd
 conn = Connection(
     host='localhost',
     port=3306,
-    user='stock',
-    password='123456',
+    user='root',
+    password='wu101402',
     autocommit=True
 )
 
 conn.select_db('stockhub')
 cursor = conn.cursor()
-user_type = ""
+user_type = 2 # 用来判断用户类型
 user_id = ""
+
+
+def test(request):
+    return render(request, "test.html")
 
 
 def login(request):
@@ -35,7 +39,6 @@ def login(request):
             tmp_type = int(info[0][0])
             tmp_type = str(tmp_type)
 
-
             global user_id
             user_id= tmp_id
             global user_type
@@ -48,26 +51,26 @@ def login(request):
 
 # search 这一部分有一点疑问 ： 我们是一个表一个function 还是中间还有一个跳转界面来决定展示哪一部分
 # (存疑 回学校跟你说)
-def search(request):
-    if request.method == 'GET':
-        return render(request, "search.html", {"user_type": user_type})
-
-    stock_code = request.POST.get('stock_code')
-    code_check = cursor.execute(f'select * from ')
-    stock_name = request.POST.get('stock_name')
-    # 这里搜索成功后就是跳转到展示信息的页面
-    if stock_code:
-        cursor.execute(f'select * from stock_basic_info where SecuCode = {stock_code}')
-        info = cursor.fetchall()
-        data = pd.DataFrameinfo()
-        return HttpResponse("以代码搜索股票")
-    if stock_name:
-        cursor.execute(f'select * from stock_basic_info where SecuCode = {stock_name}')
-        info = cursor.fetchall()
-        data = pd.DataFrameinfo()
-        return HttpResponse("以名字搜索股票")
-    else:
-        return HttpResponse("输入错误")
+# def search(request):
+#     if request.method == 'GET':
+#         return render(request, "search.html", {"user_type": user_type})
+#
+#     stock_code = request.POST.get('stock_code')
+#     code_check = cursor.execute(f'select * from ')
+#     stock_name = request.POST.get('stock_name')
+#     # 这里搜索成功后就是跳转到展示信息的页面
+#     if stock_code:
+#         cursor.execute(f'select * from stock_basic_info where SecuCode = {stock_code}')
+#         info = cursor.fetchall()
+#         data = pd.DataFrameinfo()
+#         return HttpResponse("以代码搜索股票")
+#     if stock_name:
+#         cursor.execute(f'select * from stock_basic_info where SecuCode = {stock_name}')
+#         info = cursor.fetchall()
+#         data = pd.DataFrameinfo()
+#         return HttpResponse("以名字搜索股票")
+#     else:
+#         return HttpResponse("输入错误")
 
 
 def register(request):
@@ -107,5 +110,345 @@ def delete(request):
             return HttpResponse("用户或者密码错误")
 
 
+def stock_basic_info(request):
+    if request.method == 'GET':
+        return render(request, "stock_basic_info.html")
+    else:
+        # 获取code和name
+        SecuCode = request.POST.get('SecuCode')
+        Lstknm = request.POST.get('Lstknm')
+
+        # 以code判断
+        if SecuCode:
+
+            check = cursor.execute(f"select * from stock_basic_info where SecuCode = '{SecuCode}'")
+            if check:
+                info = cursor.fetchall()
+                data = pd.DataFrame(info)
+
+                return HttpResponse("搜索成功")
+            else:
+                return render(request, "searchFail.html")
+        # 以名字判断
+        if Lstknm:
+
+            check = cursor.execute(f"select * from stock_basic_info where  Lstknm = '{Lstknm}'")
+            if check:
+                info = cursor.fetchall()
+                data = pd.DataFrame(info)
+
+                return HttpResponse("搜索成功")
+            else:
+                return render(request, "searchFail.html")
+            # 什么都没有输入 搜索失败
+        else:
+            return render(request, "searchFail.html")
+
+
+def stock_daily_data(request):
+    if request.method == 'GET':
+        if user_type >= 1:
+            return render(request, "stock_daily_data.html")
+        else:
+            return HttpResponse("失败")
+
+    else:
+        SecuCode = request.POST.get('SecuCode')
+        Lstknm = request.POST.get('Lstknm')
+        if SecuCode:
+            check = cursor.execute(f"select * from stock_daily_data where SecuCode = '{SecuCode}'")
+            if check:
+                info = cursor.fetchall()
+                data = pd.DataFrame(info)
+                print(data)
+
+                return HttpResponse("搜索成功")
+            else:
+                return render(request, "searchFail.html")
+        if Lstknm:
+            check = cursor.execute(f"select * from stock_basic_info where  Lstknm = '{Lstknm}'")
+            if check:
+                info = cursor.fetchall()
+                data = pd.DataFrame(info)
+
+                return HttpResponse("搜索成功")
+
+            else:
+                return render(request, "searchFail.html")
+
+        else:
+            return render(request, "searchFail.html")
+
+
+def stock_dividend_data(request):
+    if request.method == 'GET':
+        if user_type >= 1:
+            return render(request, "stock_dividend_data.html")
+        else:
+            return HttpResponse("失败")
+
+    else:
+        SecuCode = request.POST.get('SecuCode')
+        Lstknm = request.POST.get('Lstknm')
+        if SecuCode:
+            check = cursor.execute(f"select * from stock_dividend_data where SecuCode = '{SecuCode}'")
+            if check:
+                info = cursor.fetchall()
+                data = pd.DataFrame(info)
+                print(data)
+
+                return HttpResponse("搜索成功")
+            else:
+                return render(request, "searchFail.html")
+        if Lstknm:
+            check = cursor.execute(f"select * from stock_dividend_data where  Lstknm = '{Lstknm}'")
+            if check:
+                info = cursor.fetchall()
+                data = pd.DataFrame(info)
+
+                return HttpResponse("搜索成功")
+
+            else:
+                return render(request, "searchFail.html")
+
+        else:
+            return render(request, "searchFail.html")
+
+
+def stock_fees_data(request):
+    if request.method == 'GET':
+        if user_type >= 1:
+            return render(request, "stock_fees_data.html")
+        else:
+            return HttpResponse("失败")
+
+    else:
+        SecuCode = request.POST.get('SecuCode')
+        Lstknm = request.POST.get('Lstknm')
+        if SecuCode:
+            check = cursor.execute(f"select * from stock_fees_data where SecuCode = '{SecuCode}'")
+            if check:
+                info = cursor.fetchall()
+                data = pd.DataFrame(info)
+                print(data)
+
+                return HttpResponse("搜索成功")
+            else:
+                return render(request, "searchFail.html")
+        if Lstknm:
+            check = cursor.execute(f"select * from stock_fees_data where  Lstknm = '{Lstknm}'")
+            if check:
+                info = cursor.fetchall()
+                data = pd.DataFrame(info)
+
+                return HttpResponse("搜索成功")
+
+            else:
+                return render(request, "searchFail.html")
+
+        else:
+            return render(request, "searchFail.html")
+
+
+def stock_financial_data(request):
+    if request.method == 'GET':
+        if user_type >= 1:
+            return render(request, "stock_financial_data.html")
+        else:
+            return HttpResponse("失败")
+
+    else:
+        SecuCode = request.POST.get('SecuCode')
+        Lstknm = request.POST.get('Lstknm')
+        if SecuCode:
+            check = cursor.execute(f"select * from stock_financial_data where SecuCode = '{SecuCode}'")
+            if check:
+                info = cursor.fetchall()
+                data = pd.DataFrame(info)
+                print(data)
+
+                return HttpResponse("搜索成功")
+            else:
+                return render(request, "searchFail.html")
+        if Lstknm:
+            check = cursor.execute(f"select * from stock_financial_data where  Lstknm = '{Lstknm}'")
+            if check:
+                info = cursor.fetchall()
+                data = pd.DataFrame(info)
+
+                return HttpResponse("搜索成功")
+
+            else:
+                return render(request, "searchFail.html")
+
+        else:
+            return render(request, "searchFail.html")
+
+
+def stock_price_data(request):
+    if request.method == 'GET':
+        if user_type >= 1:
+            return render(request, "stock_price_data.html")
+        else:
+            return HttpResponse("失败")
+
+    else:
+        SecuCode = request.POST.get('SecuCode')
+        Lstknm = request.POST.get('Lstknm')
+        if SecuCode:
+            check = cursor.execute(f"select * from stock_price_data where SecuCode = '{SecuCode}'")
+            if check:
+                info = cursor.fetchall()
+                data = pd.DataFrame(info)
+                print(data)
+
+                return HttpResponse("搜索成功")
+            else:
+                return render(request, "searchFail.html")
+        if Lstknm:
+            check = cursor.execute(f"select * from stock_price_data where  Lstknm = '{Lstknm}'")
+            if check:
+                info = cursor.fetchall()
+                data = pd.DataFrame(info)
+
+                return HttpResponse("搜索成功")
+
+            else:
+                return render(request, "searchFail.html")
+
+        else:
+            return render(request, "searchFail.html")
+
+
+def stock_ratios_data(request):
+    if request.method == 'GET':
+        if user_type >= 1:
+            return render(request, "stock_ratios_data.html")
+        else:
+            return HttpResponse("失败")
+
+    else:
+        SecuCode = request.POST.get('SecuCode')
+        Lstknm = request.POST.get('Lstknm')
+        if SecuCode:
+            check = cursor.execute(f"select * from stock_ratios_data where SecuCode = '{SecuCode}'")
+            if check:
+                info = cursor.fetchall()
+                data = pd.DataFrame(info)
+                print(data)
+
+                return HttpResponse("搜索成功")
+            else:
+                return render(request, "searchFail.html")
+        if Lstknm:
+            check = cursor.execute(f"select * from stock_ratios_data where  Lstknm = '{Lstknm}'")
+            if check:
+                info = cursor.fetchall()
+                data = pd.DataFrame(info)
+
+                return HttpResponse("搜索成功")
+
+            else:
+                return render(request, "searchFail.html")
+
+        else:
+            return render(request, "searchFail.html")
+
+
+def stock_return_data(request):
+    if request.method == 'GET':
+        if user_type >= 1:
+            return render(request, "stock_return_data.html")
+        else:
+            return HttpResponse("失败")
+
+    else:
+        SecuCode = request.POST.get('SecuCode')
+        Lstknm = request.POST.get('Lstknm')
+        if SecuCode:
+            check = cursor.execute(f"select * from stock_return_data where SecuCode = '{SecuCode}'")
+            if check:
+                info = cursor.fetchall()
+                data = pd.DataFrame(info)
+                print(data)
+
+                return HttpResponse("搜索成功")
+            else:
+                return render(request, "searchFail.html")
+        if Lstknm:
+            check = cursor.execute(f"select * from stock_return_data where  Lstknm = '{Lstknm}'")
+            if check:
+                info = cursor.fetchall()
+                data = pd.DataFrame(info)
+
+                return HttpResponse("搜索成功")
+
+            else:
+                return render(request, "searchFail.html")
+
+        else:
+            return render(request, "searchFail.html")
+
+
+def stock_shares_data(request):
+    if request.method == 'GET':
+        if user_type >= 1:
+            return render(request, "stock_shares_data.html")
+        else:
+            return HttpResponse("失败")
+
+    else:
+        SecuCode = request.POST.get('SecuCode')
+        Lstknm = request.POST.get('Lstknm')
+        if SecuCode:
+            check = cursor.execute(f"select * from stock_shares_data where SecuCode = '{SecuCode}'")
+            if check:
+                info = cursor.fetchall()
+                data = pd.DataFrame(info)
+                print(data)
+
+                return HttpResponse("搜索成功")
+            else:
+                return render(request, "searchFail.html")
+        if Lstknm:
+            check = cursor.execute(f"select * from stock_shares_data where  Lstknm = '{Lstknm}'")
+            if check:
+                info = cursor.fetchall()
+                data = pd.DataFrame(info)
+
+                return HttpResponse("搜索成功")
+
+            else:
+                return render(request, "searchFail.html")
+
+        else:
+            return render(request, "searchFail.html")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def home(request):
     return render(request, 'home.html')
+
+
+# 展示所有可以搜索的目录
+def search_list(request):
+    return render(request, "search_list.html")
